2f73e6b5277ee8db5d17c3e67d74ad13825375b2
Cloud Mu <muyunsong@gmail.com>
2016-08-19 22:49:45 +0800

    Add wui command line.

---
+++ bin/wui.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+var yeoman = require('yeoman-environment');
+var env = yeoman.createEnv();
+
+env.lookup(function() {
+  env.run('wdf:web name', function(err) {
+    console.log('done');
+  });
+});
