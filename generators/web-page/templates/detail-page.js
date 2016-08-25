161cb31fd7d24ac6a99852a09bb4d21a507a65b5
Cloud Mu <muyunsong@gmail.com>
2016-08-25 08:56:29 +0800

    Add more logic.

--- generators/web-page/templates/detail-page.js
+++ generators/web-page/templates/detail-page.js
@@ -1,6 +1,52 @@
 'use strict';
-
+// Constants
+var META = {
+  id: { label: '用户ID' },
+  name: { label: '用户姓名', required: true, maxLength: 20 },
+  email: { label: 'Email', type: 'email', required: true, maxLength: 11 },
+  age: { label: '年龄', required: true, type: 'number' },
+  birthday: { label: '生日', type: 'date', format: 'YYYY-DD-MM' },
+  status: {
+    label: '用户状态',
+    type: 'select',
+    options: [{
+      label: '未开户',
+      value: 0
+    }, {
+      label: '未激活',
+      value: 1
+    }, {
+      label: '正常',
+      value: 2
+    }, {
+      label: '已销户',
+      value: 3
+    }]
+  }
+};
+var EDIT_FIELDS = ['name', 'age', 'birthday', 'email', 'status'];
+// Callback Functions
+var submitData = function (item, etag) {
+  WUI.ajax({
+    url: '/sample/web/v1/users/' + WUI.link().id,
+    jsonData: $.extend(item, {
+      _etag: etag
+    })
+  }).done(function () {
+    WUI.alert.create({
+      message: '成功',
+      success: true
+    });
+    /**
+    setTimeout(function() {
+      window.close();
+    }, 2000);
+    */
+  });
+};
+// Load Page
 WUI.ready = function () {
+  // TODO: Need ContentHeader here
   $('.content-header').html(WUI.templates['content-header']({
     paths: [{
       name: '列表页面',
@@ -10,70 +56,21 @@ WUI.ready = function () {
     }]
   }));
 
-  var meta = {
-    id: {
-      label: '用户ID'
-    },
-    name: {
-      label: '用户姓名',
-      required: true,
-      maxLength: 20,
-      edit: true
-    },
-    email: {
-      label: 'Email',
-      type: 'email',
-      required: true,
-      maxLength: 11,
-      edit: true
-    },
-    age: {
-      label: '年龄',
-      required: true,
-      type: 'number',
-      edit: true
-    },
-    birthday: {
-      label: '生日',
-      type: 'date',
-      edit: true
-    },
-    status: {
-      label: '用户状态',
-      type: 'select',
-      options: [{
-        label: '未开户',
-        value: 0
-      }, {
-        label: '未激活',
-        value: 1
-      }, {
-        label: '正常',
-        value: 2
-      }, {
-        label: '已销户',
-        value: 3
-      }]
-    }
-  };
-
   WUI.ajax({
     url: '/sample/web/v1/users/' + WUI.link().id,
     method: 'GET'
   }).done(function (resp) {
     var user = resp.data;
+    var editResource = WUI.getResource('/edit');
 
     WUI.dataDialog.create({
       $el: $('#detail-info'),
-      meta: meta,
+      meta: META,
       list: user,
-      fields: ['name', 'email', 'age', 'birthday', 'status'],
-      onConfirm: function (userInfo) {
-        console.log(userInfo);
-      },
-      onCancel: function () {
-        window.close();
-      }
+      fields: EDIT_FIELDS,
+      confirmButton: editResource && editResource.resourceDisplayName,
+      onConfirm: submitData.bind(null, user, resp.etag),
+      onCancel: window.close
     });
   });
 };
--- generators/web-project/templates/app/scripts/sample/list-page.js
+++ generators/web-project/templates/app/scripts/sample/list-page.js
@@ -1,132 +1,147 @@
 'use strict';
+// Constants
+var PAGE_SIZE = 20;
+var META = {
+  id: { label: '用户ID' },
+  name: { label: '用户姓名', required: true, maxLength: 20 },
+  email: { label: 'Email', type: 'email', required: true, maxLength: 11 },
+  age: { label: '年龄', required: true, type: 'number' },
+  birthday: { label: '生日', type: 'date', format: 'YYYY-DD-MM' },
+  status: {
+    label: '用户状态',
+    type: 'select',
+    options: [{
+      label: '未开户',
+      value: 0
+    }, {
+      label: '未激活',
+      value: 1
+    }, {
+      label: '正常',
+      value: 2
+    }, {
+      label: '已销户',
+      value: 3
+    }]
+  }
+};
+var TABLE_FIELDS = ['id', 'name', 'age', 'birthday', 'email', 'status'];
+var FILTER_FIELDS = ['name', 'age', 'birthday', 'status'];
+var EDIT_FIELDS = ['name', 'age', 'birthday', 'email', 'status'];
+// Callback functions
+var createData = function (item) {
+  WUI.dataDialog.create({
+    $el: $('.data-dialog'),
+    meta: META,
+    list: item,
+    buttonHide: false,
+    fields: EDIT_FIELDS,
+    onConfirm: function (itemInfo) {},
+    onCancel: function () {}
+  });
+};
 
+var editData = function (item) {
+  window.open('detail-page.html?id=' + item.id);
+};
+
+var deleteData = function (item) {
+  // TODO: Need Confirm here
+  console.log('delete: ' + item);
+};
+
+var activeData = function (items) {
+  console.log('active: ' + items);
+};
+
+var deactiveData = function (items) {
+  console.log('deactive: ' + items);
+};
+
+var queryData = function (options) {
+  var editResource = WUI.getResource('/edit');
+  var deleteResource = WUI.getResource('/delete');
+
+  WUI.ajax({
+    url: '/sample/web/v1/users',
+    method: 'GET',
+    jsonData: {
+      currentPage: options.currentPage,
+      pageSize: options.pageSize
+    }
+  }).then(function (resp) {
+    WUI.DataTable.create({
+      $el: $('.data-table'),
+      meta: META,
+      fields: TABLE_FIELDS,
+      list: resp.list,
+      operations: [{
+        name: editResource && editResource.resourceDisplayName,
+        callback: editData
+      }, {
+        name: deleteResource && deleteResource.resourceDisplayName,
+        callback: deleteData
+      }],
+      groups: [{
+        name: '启用',
+        className: 'btn btn-xs btn-success',
+        id: 'btn-success',
+        callback: activeData
+      }, {
+        name: '停用',
+        className: 'btn btn-xs btn-danger',
+        id: 'btn-danger',
+        callback: deactiveData
+      }]
+    });
+
+    WUI.DataPaginator.create({
+      $el: $('.data-paginator'),
+      currentPage: options.currentPage,
+      total: resp.total,
+      pageSize: options.pageSize,
+      onSwitchPage: function (currentPage) {
+        queryData({
+          currentPage: currentPage,
+          pageSize: options.pageSize
+        });
+      }
+    });
+  });
+};
+// Load Page
 WUI.ready = function () {
+  var queryResource = WUI.getResource('/query');
+  var addResource = WUI.getResource('/add');
+
+  // TODO: Need ContentHeader here
   $('.content-header').html(WUI.templates['content-header']({
     paths: [{
       name: '列表页面'
     }]
   }));
 
-  var meta = {
-    id: {
-      label: '用户ID'
-    },
-    name: {
-      label: '用户姓名',
-      required: true,
-      maxLength: 20
-    },
-    email: {
-      label: 'Email',
-      type: 'email',
-      required: true,
-      maxLength: 11
-    },
-    age: {
-      label: '年龄',
-      required: true,
-      type: 'number'
-    },
-    birthday: {
-      label: '生日',
-      type: 'date',
-      format: 'YYYY-DD-MM'
-    },
-    status: {
-      label: '用户状态',
-      type: 'select',
-      options: [{
-        label: '未开户',
-        value: 0
-      }, {
-        label: '未激活',
-        value: 1
-      }, {
-        label: '正常',
-        value: 2
-      }, {
-        label: '已销户',
-        value: 3
-      }],
-      createHide: true
-    }
-  };
-
-  var editData = function(item) {
-    window.open('detail-page.html?id=' + item.id);
-  };
-
-  var resetPassword = function(item) {
-    console.log('reset', item);
-  };
-
-  var queryData = function(options) {
-    WUI.ajax({
-      url: '/sample/web/v1/users?_start=' + options.currentPage * options.pageSize + '&_limit=' + options.pageSize,
-      method: 'GET',
-      jsonData: {
-        currentPage: options.currentPage,
-        pageSize: options.pageSize
-      }
-    }).then(function(resp) {
-      WUI.DataTable.create({
-        $el: $('.data-table'),
-        meta: meta,
-        fields: ['id', 'name', 'age', 'birthday', 'email', 'status'],
-        list: resp.list,
-        operations: [{
-          name: '编辑',
-          callback: editData
-        }, {
-          name: '重置密码',
-          callback: resetPassword
-        }],
-        groups: [{
-          name: '启用',
-          className:'btn btn-xs btn-success',
-          id:'btn-success',
-          callback: editData
-        }, {
-          name: '停用',
-          className:'btn btn-xs btn-danger',
-          id:'btn-danger',
-          callback: resetPassword
-        }]
-      });
-
-      WUI.DataPaginator.create({
-        $el: $('.data-paginator'),
-        currentPage: options.currentPage,
-        total: resp.total,
-        pageSize: options.pageSize,
-        onSwitchPage: function(currentPage) {
-          queryData({
-            currentPage: currentPage,
-            pageSize: options.pageSize
-          });
-        }
-      });
-    });
-  };
-
   WUI.DataFilter.create({
     $el: $('.data-filter'),
-    meta: meta,
-    fields: ['mobile', 'userName', 'status'],
-    onFilter: function(params) {
+    meta: META,
+    fields: FILTER_FIELDS,
+    queryButton: queryResource && queryResource.resourceDisplayName,
+    onFilter: function (params) {
       queryData($.extend(params, {
         currentPage: 0,
-        pageSize: 1
+        pageSize: PAGE_SIZE
       }));
-    }
+    },
+    addButton: addResource && addResource.resourceDisplayName,
+    addFunc: createData
   });
 
   queryData({
     currentPage: 0,
     pageSize: 20
   });
-};
-
+}
+// Keep this function
 $(function () {
   WUI.init({
     system: 'sample'
